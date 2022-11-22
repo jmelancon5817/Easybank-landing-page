b4d17696e247f9f2dd84fab80c42cbfc75be0942
Jacob Melancon <jmelancon5817@bellsouth.net>
2022-11-22 11:17:53 -0600

    Add animated mobile menu

--- app/script.js
+++ app/script.js
@@ -1,17 +1,25 @@
 const hamburgerButton = document.querySelector("#hamburger");
 const header = document.querySelector(".header");
+const body = document.querySelector("body");
 const overlay = document.querySelector(".overlay");
+const fadeElements = document.querySelectorAll(".has-fade");
 
 hamburgerButton.addEventListener("click", function () {
   console.log("click button");
 
   if (header.classList.contains("open")) {
+    body.classList.remove("no_scroll");
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    fadeElements.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
   } else {
+    body.classList.add("no_scroll");
     header.classList.add("open");
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    fadeElements.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
 });
